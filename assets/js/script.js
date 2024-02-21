e613091c5f2327210173e7cf3fe7fe6006ae1e9f
Kavini Wickramasooriya <149506745+KaviniPramudika@users.noreply.github.com>
2024-02-21 13:56:28 +0530

    portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,3 +73,25 @@ function toggleMenu() {
   var menu = document.querySelector('.menu');
   menu.classList.toggle('active');
 }
+
+
+
+
+
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function moveSlide(n) {
+  showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
